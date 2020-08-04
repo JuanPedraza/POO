ffd148aac4c55d6d7492a507e9ad2700a3311a4c
Juan Pedraza <juan_pedraza@outlook.com>
2020-08-03 21:39:47 -0500

    Creating more classes and to improve the synthax

--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,8 @@
 class  Main {
     public static void main(String[] args) {
-        
+        Car car = new Car("AMQ123", new Account("Marcus Fenix", "AND123"));
+        car.passenger = 4;
+        car.printDataCar();        
+
     }
 }
---
+++ js/index.js
@@ -0,0 +1,3 @@
+const car = new Car("AW456", new Account("MARCUS FENIX", "AQWE123"));
+car.passenger = 4;
+car.printDataCar();
